7585890274b10095a2356b1ecd53bb2aa7c88415
losalmson <mialo.salmson@gmail.com>
2024-05-29 14:24:06 +0200

    added mounting to await api call

--- src/components/People.vue
+++ src/components/People.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import { IPeopleState } from '../models/IPeopleState';
+import { getCoworkers } from '../services/peopleService';
 
 const state = ref<IPeopleState>({
     coworkers: [],
 });
 
+onMounted(async () => {
+    state.value.coworkers = await getCoworkers();
+});
 </script>
