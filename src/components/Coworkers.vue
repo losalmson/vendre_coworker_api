48e3b76c8a9b67c31ea892c89d42f1b6619a3012
losalmson <mialo.salmson@gmail.com>
2024-05-29 15:26:41 +0200

    working on displaying coworkers

---
+++ src/components/Coworkers.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ICoworker } from '../models/ICoworker';
+import Coworker from './Coworker.vue';
+
+
+interface CoworkersProp {
+    coworkers: ICoworker[];
+}
+
+defineProps<CoworkersProp>();
+
+</script>
+
+<template>
+    <section class="coworkers">
+        <Coworker 
+        v-for="coworker in coworkers"
+        :coworker="coworker"
+        :key="coworker.id"/>
+    </section>
+</template>
