cf7f771875e1d5dd22423c4fb91e854c0ca96034
losalmson <mialo.salmson@gmail.com>
2024-05-29 14:22:10 +0200

    set initial state with an empty list of IPeople aka coworkers

---
+++ src/components/People.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { IPeopleState } from '../models/IPeopleState';
+
+const state = ref<IPeopleState>({
+    coworkers: [],
+});
+
+</script>
