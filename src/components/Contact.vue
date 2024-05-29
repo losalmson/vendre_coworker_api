a46e6365cb08a06bcac319add8a0bf6e84702075
losalmson <mialo.salmson@gmail.com>
2024-05-30 00:24:24 +0200

    Added empty components for Contact page and Pagnation functionality

---
+++ src/components/Contact.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ICoworker } from "../models/ICoworker";
+
+interface ContactProps {
+    coworker: ICoworker;
+}
+defineProps<ContactProps>();
+</script>
+
+<template>
+</template>
