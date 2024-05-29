0b9588fe55db1143461e69cd04b9ee3d373ab16f
losalmson <mialo.salmson@gmail.com>
2024-05-29 14:00:50 +0200

    added base URL for axios call and a function to get list of coworkers from the api + empty Interface for Coworker

---
+++ src/services/peopleService.ts
@@ -0,0 +1,8 @@
+
+import { get } from "./serviceBase";
+
+const BASE_URL = "https://reqres.in/api/users";
+
+export const getCoworkers = async(): Promise<ICoworker[]> => {
+    return await get<ICoworker[]>(BASE_URL);
+};
