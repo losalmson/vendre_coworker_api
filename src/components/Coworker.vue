89f14bc5e09cc6cb34f47f03bf1a71f70ecb555d
losalmson <mialo.salmson@gmail.com>
2024-05-29 15:18:25 +0200

    🚧 started working on script and template

---
+++ src/components/Coworker.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ICoworker } from "../models/ICoworker";
+
+interface CoworkerProps {
+    coworker: ICoworker[];
+}
+
+defineProps<CoworkerProps>();
+ 
+ defineEmits<{
+    (e:)
+ }>();
+
+</script>
+
+<template>
+    <article class="coworker">
+        <h3>{{ coworker. }}</h3>
+    </article>
+</template>
