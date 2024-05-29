1cdd08d780cbd5280d81534e99b446d60edc0b46
losalmson <mialo.salmson@gmail.com>
2024-05-29 23:28:18 +0200

    💄added som styling to the coworker presentation, such as fonts and brand colors

--- src/components/Coworker.vue
+++ src/components/Coworker.vue
@@ -14,8 +14,28 @@ defineProps<CoworkerProps>();
 
 <template>
     <article class="coworker">
-        <img :src="coworker.avatar" :alt="coworker.email"/>
-        <h3>{{ coworker.first_name}} {{ coworker.last_name }}</h3>
+        <img class="avatar-image" :src="coworker.avatar" :alt="coworker.email" loading="lazy"/>
+        <h3 class="coworker-name">{{ coworker.first_name}} {{ coworker.last_name }}</h3>
         <span> {{ coworker.email }}</span>
     </article>
 </template>
+
+<style scoped>
+
+.avatar-image {
+   border-radius: 50%;
+   transition: box-shadow 0.3s ease;
+}
+
+.avatar-image:hover {
+    box-shadow: inset 0 0 0 2px #ffffff, 0 0 0 2px #5432ED;
+}
+
+.coworker-name {
+    color: #5432ED
+}
+
+.coworker-name:hover {
+    color: #AE9EF6
+}
+</style>
