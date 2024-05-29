110a200280180028c276480e26200f5b7c36b8a5
losalmson <mialo.salmson@gmail.com>
2024-05-29 14:47:43 +0200

    ➕installed axios

--- src/services/peopleService.ts
+++ src/services/peopleService.ts
@@ -1,8 +1,9 @@
 import { ICoworker } from "../models/ICoworker";
 import { get } from "./serviceBase";
 
-const BASE_URL = "https://reqres.in/api/users";
+const BASE_URL = 
+"https://reqres.in/api/users";
 
-export const getCoworkers = async(): Promise<ICoworker[]> => {
+export const getCoworkers = async (): Promise<ICoworker[]> => {
     return await get<ICoworker[]>(BASE_URL);
 };
