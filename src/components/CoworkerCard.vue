7f99636862a5711d36095ba5fe045d76030cd842
losalmson <mialo.salmson@gmail.com>
2024-05-30 20:25:41 +0200

    implemented component for mobile with coworker cards and styling

---
+++ src/components/CoworkerCard.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { ICoworker } from "../models/ICoworker";
+
+interface CoworkerCardProps {
+    coworker: ICoworker;
+}
+
+defineProps<CoworkerCardProps>();
+ 
+ defineEmits<{
+ }>();
+
+</script>
+
+<template>
+    <article class="coworker-card">
+        <img class="avatar-image" :src="coworker.avatar" :alt="coworker.email" loading="lazy"/>
+        <div class="coworker-info">
+        <h3 class="coworker-name">{{ coworker.first_name}} {{ coworker.last_name }}</h3>
+        <a :href="'mailto:' + coworker.email" class="coworker-email">{{ coworker.email }}</a>
+        </div>
+    </article>
+</template>
+
+<style scoped>
+.coworker-card {
+  display: flex;
+  align-items: center;
+  padding: 16px;
+  border-radius: 8px;
+  border: 1px solid #D8D9D9;
+  box-sizing: border-box;
+  background: #fff;
+  margin-top: 16px;
+  transition: box-shadow 0.3s ease;
+  max-width: 300px;
+}
+
+.coworker-card:hover,
+.coworker-card.active {
+  box-shadow: 0 0 8px 2px rgba(84, 50, 237, 0.3);
+}
+
+.avatar-image {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  border: 3px solid #5432ED;
+  margin-right: 16px;
+  transition: box-shadow 0.3s ease;
+}
+
+.coworker-info {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.coworker-name {
+  color: #5432ED;
+  margin: 0;
+  font-size: 1.2em;
+}
+
+.coworker-email {
+  color: #000000;
+  font-size: 0.9em;
+  margin-top: 4px;
+  text-decoration: underline;
+}
+</style>
--- src/components/Coworkers.vue
+++ src/components/Coworkers.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue';
 import { ICoworker } from '../models/ICoworker';
 import Coworker from './Coworker.vue';
+import CoworkerCard from './CoworkerCard.vue';
+
 
 interface CoworkersProp {
     coworkers: ICoworker[];
@@ -8,14 +11,39 @@ interface CoworkersProp {
 
 defineProps<CoworkersProp>();
 
+const isMobile = ref(window.innerWidth <= 768);
+
+const handleResize = () => {
+    isMobile.value = window.innerWidth <= 768;
+};
+
+onMounted(() => {
+    window.addEventListener('resize', handleResize);
+});
+
+onUnmounted(() => {
+    window.removeEventListener('resize', handleResize);
+})
+
 </script>
 
 <template>
-    <section class="coworkers">
+    <section :class="{ coworkers: true, 'coworkers-mobile': isMobile }">
+    <template v-if="!isMobile">
         <Coworker 
+        v-if="!isMobile"
         v-for="coworker in coworkers"
         :coworker="coworker"
         :key="coworker.id"/>
+    </template>
+    <template v-else>
+        <div class="coworker-card-container">
+        <CoworkerCard
+        v-for="coworker in coworkers"
+        :coworker="coworker"
+        :key="coworker.id" />
+        </div>
+    </template>
     </section>
 </template>
 
@@ -26,11 +54,17 @@ defineProps<CoworkersProp>();
   flex-wrap: wrap;
   gap: 16px;
   justify-content: space-between;
-  margin: 50px;
+  margin: 50px auto;
+}
+
+.coworkers-mobile {
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* Center items horizontally */
 }
 
 .coworker {
-    flex: 0 0 calc(33.33% - 16px);
-    box-sizing: border-box;
+  flex: 0 0 calc(33.33% - 16px);
+  box-sizing: border-box;
 }
 </style>
