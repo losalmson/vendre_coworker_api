2932f8f7f5df6ce9e4b92294fe02a99947ffeced
losalmson <mialo.salmson@gmail.com>
2024-05-31 11:11:05 +0200

    started working on pagination, but want to move it into it's own component

--- src/components/People.vue
+++ src/components/People.vue
@@ -5,10 +5,31 @@ import Coworkers from './Coworkers.vue';
 import { ICoworker } from '../models/ICoworker';
 
 const coworkers = ref<ICoworker[]>([]);
+const currentPage = ref(1);
+const totalPages = ref(0);
+
+const fetchCoworkers = async (page:number) => {
+    const response = await getCoworkers(page);
+    coworkers.value = response.data;
+    totalPages.value = response.total_pages;
+};
+
+const nextPage = () => {
+    if (currentPage.value < totalPages.value) {
+        currentPage.value++;
+        fetchCoworkers(currentPage.value);
+    }
+};
+
+const previousPage = () => {
+    if (currentPage.value > 1) {
+        currentPage.value--;
+        fetchCoworkers(currentPage.value)
+    }
+}
 
 onMounted(async () => {
-    const response = await getCoworkers(1);
-    coworkers.value = response.data;
+   fetchCoworkers(currentPage.value);
 });
 </script>
 
@@ -27,6 +48,15 @@ onMounted(async () => {
     <Coworkers
     :coworkers="coworkers"
     />
+    <div class="pagination">
+        <button @click="previousPage" :disabled="currentPage === 1">Previous</button>
+        <button 
+            v-for="page in totalPages" 
+            :key="page"
+            :class="{ active: page === currentPage }"
+        >{{ page }}</button>
+        <button @click="nextPage" :disabled="currentPage === totalPages">Next</button>
+    </div>
 </template>
 
 <style scoped>
@@ -34,5 +64,29 @@ onMounted(async () => {
 .info-text {
 max-width: 600px;
 margin: 0 auto;
+}
+
+.pagination {
+  display: inline-block;
+}
+
+.pagination button {
+  color: #000000;
+  padding: 8px 16px;
+  text-decoration: none;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+
+.pagination button.active {
+  background-color: #674AEF;
+  color: #FFFFFF;
+  border-radius: 5px;
+}
+
+.pagination button:hover:not(.active) {
+  background-color: #EDEBFE;
+  border-radius: 5px;
 }
 </style>
--- src/services/peopleService.ts
+++ src/services/peopleService.ts
@@ -1,9 +1,9 @@
 import { IApiResponse } from "../models/IApiResponse";
 import { get } from "./serviceBase";
 
-const BASE_URL = 
-"https://reqres.in/api/users";
+const BASE_URL = "https://reqres.in/api/users";
 
-export const getCoworkers = async (): Promise<IApiResponse> => {
-    return await get<IApiResponse>(BASE_URL);
+export const getCoworkers = async (page: number): Promise<IApiResponse> => {
+    const url = `${BASE_URL}?page=${page}`;
+    return await get<IApiResponse>(url);
 };
