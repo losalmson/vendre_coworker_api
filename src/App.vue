e9118e6ded3ff3f48058a88ab171144efe379682
losalmson <mialo.salmson@gmail.com>
2024-06-02 18:12:54 +0200

    added pulsating vendre loader

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import Loader from './components/Loader.vue'; 
 import People from './components/People.vue';
+
+const isLoading = ref(true);
+const people = ref(false);
+
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false;
+    people.value = true;
+  }, 3000);
+});
 </script>
 
 <template>
-        <People />
+        <Loader v-if="isLoading" />
+        <People v-else="people" />
 </template>
 
 <style scoped>
---
+++ src/components/Loader.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="loader-background">
+        <div class="loader">
+            <img src="/src/assets/vendre-optimizers_logo.png" alt="Loading"/>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.loader-background {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #edebfe;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9999;
+  transition: opacity 0.5s ease, transform 0.5s ease;
+}
+
+.loader-background-leave-active {
+  opacity: 0;
+  transform: translateY(-100%);
+}
+
+.loader img {
+  width: auto; 
+  height: auto;
+  max-width: 100px;
+  max-height: 100px;
+  animation: pulsate 1s infinite alternate;
+}
+
+@keyframes pulsate {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(1.2);
+  }
+}
+</style>
